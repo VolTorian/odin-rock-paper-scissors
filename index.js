dc1c8bf5e7acd40ad5d596ca079b4deee5724fbc
VolTorian <voltorian.social@gmail.com>
2023-09-05 18:19:21 -0700

    Multiround game function complete

--- index.js
+++ index.js
@@ -7,50 +7,95 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoiceRaw, computerChoice) {
+    if (!playerChoiceRaw) {
+        console.log("Make a selection!");
+        return;
+    }
+
     let playerChoice = playerChoiceRaw.toLowerCase();
     
     if (!CHOICES.includes(playerChoice)) {
         console.log("Invalid selection!");
         
-        return "invalid";
+        return;
     }
     
     console.log("You chose " + playerChoice + " against " + computerChoice + ".")
 
     if (playerChoice === computerChoice) {
-        console.log("It's a tie!");
+        return "Tie";
     }
     else {
         if (playerChoice === "rock") {
             if (computerChoice === "paper") {
-                console.log("You lose!");
+                return "ComputerWin";
             }
-
+            
             if (computerChoice === "scissors") {
-                console.log("You win!");
+                return "PlayerWin";
             }
             
         }
         else if (playerChoice === "paper") {
             if (computerChoice === "scissors") {
-                console.log("You lose!");
+                return "ComputerWin";
             }
 
             if (computerChoice === "rock") {
-                console.log("You win!");
+                return "PlayerWin";
             }
         }
         else if (playerChoice === "scissors") {
             if (computerChoice === "rock") {
-                console.log("You lose!");
+                return "ComputerWin";
             }
 
             if (computerChoice === "paper") {
-                console.log("You win!");
+                return "PlayerWin";
             }
         }
     }
 }
 
-let playerChoice = prompt("Enter your selection for rock paper scissors:");
-playRound(playerChoice, getComputerChoice());
+function game() {
+    let playerPoints = 0;
+    let computerPoints = 0;
+
+    for (let i = 0; i < 5; i++) {
+        console.log("Round " + (i + 1));
+
+        let playerChoice = prompt("Enter your selection for rock paper scissors:");
+        let result = playRound(playerChoice, getComputerChoice());
+
+        if (!result) {
+            console.log("Bad round! Redo it!");
+            i--;
+        }
+        if (result === "Tie") {
+            console.log("You tied that round!")
+        }
+        if (result === "PlayerWin") {
+            console.log("You won that round!")
+            playerPoints++;
+        }
+        else if (result === "ComputerWin") {
+            console.log("You lost that round!")
+            computerPoints++;
+        }
+
+        console.log("Your points: " + playerPoints);
+        console.log("Computer points: " + computerPoints);
+    }
+
+    if (playerPoints === computerPoints) {
+        console.log("You tied the game!")
+    }
+    else if (playerPoints > computerPoints) {
+        console.log("You won the game!");
+    }
+    else if (playerPoints < computerPoints) {
+        console.log("You lost the game!");
+    }
+}
+
+game();
