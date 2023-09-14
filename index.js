85e3c2a5b01ccd1bba2e7c8fba98212cd36a9b67
VolTorian <voltorian.social@gmail.com>
2023-09-14 15:01:45 -0700

    game logic mostly finished

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 console.log("Welcome to rock paper scissors!");
 
 const CHOICES = ["rock", "paper", "scissors"];
+const playerPointsDisplay = document.querySelector(".score.player");
+const computerPointsDisplay = document.querySelector(".score.computer");
+const roundDisplay = document.querySelector(".round-number");
+let playerPoints = 0;
+let computerPoints = 0;
+let round = 1;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", playRound));
+
 
 function getComputerChoice() {
     return CHOICES[Math.floor(Math.random() * 3)];
@@ -9,51 +19,70 @@ function getComputerChoice() {
 function playRound(e) {
     let playerChoice = e.target.id;
     let computerChoice = getComputerChoice();
-    //
     const roundResult = document.querySelector(".round-result");
     
-    console.log(`You chose ${playerChoice} against ${computerChoice}`)
+    console.log(`You chose ${playerChoice} against ${computerChoice}.`)
     roundResult.textContent = `You chose ${playerChoice} against ${computerChoice}.`;
 
     if (playerChoice === computerChoice) {
-        roundResult.textContent += " You tied the round.";
-        return "Tie";
+        roundResult.textContent += " You tied the round. Redo it.";;
     }
     else {
         if (playerChoice === "rock") {
             if (computerChoice === "paper") {
                 roundResult.textContent += " You lost the round.";
-                return "ComputerWin";
+                computerPoints++;
             }
             
             if (computerChoice === "scissors") {
                 roundResult.textContent += " You won the round.";
-                return "PlayerWin";
+                playerPoints++;
             }
             
         }
         else if (playerChoice === "paper") {
             if (computerChoice === "scissors") {
                 roundResult.textContent += " You lost the round.";
-                return "ComputerWin";
+                computerPoints++;
             }
 
             if (computerChoice === "rock") {
                 roundResult.textContent += " You won the round.";
-                return "PlayerWin";
+                playerPoints++;
             }
         }
         else if (playerChoice === "scissors") {
             if (computerChoice === "rock") {
                 roundResult.textContent += " You lost the round.";
-                return "ComputerWin";
+                computerPoints++;
             }
 
             if (computerChoice === "paper") {
                 roundResult.textContent += " You won the round.";
-                return "PlayerWin";
+                playerPoints++;
             }
         }
+        round++;
+        console.log(playerPoints);
+        console.log(computerPoints);
+        playerPointsDisplay.textContent = `Player: ${playerPoints}`;
+        computerPointsDisplay.textContent = `Computer: ${computerPoints}`;
+    }
+    if (round > 5) {
+        gameEnd();
+    }
+}
+
+function gameEnd() {
+    console.log("Game finished");
+    if (playerPoints > computerPoints) {
+        console.log("Player wins");
+    }
+    else if (computerPoints > playerPoints) {
+        console.log("Computer wins");
+    }
+    else {
+        console.log("O.o wat there should not be a tie in a best of 5");
     }
 }
 
@@ -67,35 +96,10 @@ function playerSelect(e) {
 }
 
 function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
-    const buttons = document.querySelectorAll("button");
-    buttons.forEach((button) => button.addEventListener("click", playRound));
+
     
     for (let i = 0; i < 1; i++) {
         console.log("Round " + (i + 1));
-
-        // let playerChoice = prompt("Enter your selection for rock paper scissors:");
-        // let result = playRound(playerChoice, getComputerChoice());
-
-        // if (!result) {
-        //     console.log("Bad round! Redo it!");
-        //     i--;
-        // }
-        // if (result === "Tie") {
-        //     console.log("You tied that round!")
-        // }
-        // if (result === "PlayerWin") {
-        //     console.log("You won that round!")
-        //     playerPoints++;
-        // }
-        // else if (result === "ComputerWin") {
-        //     console.log("You lost that round!")
-        //     computerPoints++;
-        // }
-
-        // console.log("Your points: " + playerPoints);
-        // console.log("Computer points: " + computerPoints);
     }
 
     if (playerPoints === computerPoints) {
